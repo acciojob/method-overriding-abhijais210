b428028ff7460302aea700d3162f4a24d11afb7f
abhijais210 <117642669+abhijais210@users.noreply.github.com>
2023-03-16 12:22:45 +0530

    overriden check again

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,4 +6,15 @@ public class Main {
         System.out.println(obj.meth());
         System.out.println(obj.meth());
     }
+
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+        static class B extends A {
+            public String meth() {
+                return "Method is overridden in Extendend class B";
+            }
+        }
 }
