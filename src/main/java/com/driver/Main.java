1d69dd96f71f5c4b8ac8f699acff38eb19d0acc4
abhijais210 <117642669+abhijais210@users.noreply.github.com>
2023-03-16 12:13:18 +0530

    overriden check again

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,15 +6,4 @@ public class Main {
         System.out.println(obj.meth());
         System.out.println(obj.meth());
     }
-
-}
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
 }
